780f5bd7dbdf84d9b7f560daea2c440624f233b8
dsh <d.henty@epcc.ed.ac.uk>
2021-04-20 11:01:23 +0100

    Updated mpiloc.c

--- mpiloc.c
+++ mpiloc.c
@@ -1,8 +1,13 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <string.h>
+#include <sched.h>
+#include <unistd.h>
 #include <mpi.h>
 
-#include "utilities.h"
+void printlocation(char *prog);
+void cpuset_to_cstr(cpu_set_t *mask, char *str);
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +15,7 @@ int main(int argc, char *argv[])
 
   MPI_Init(&argc, &argv);
 
-  cmd  =argv[0];
+  cmd = argv[0];
   prg = cmd + strlen(cmd) - 1;
 
   while(prg > cmd && *(prg-1) != '/') prg--;
@@ -18,4 +23,62 @@ int main(int argc, char *argv[])
   printlocation(prg);
 
   MPI_Finalize();
+}
+
+
+void printlocation(char *prog)
+{
+    int rank, namelen;
+    char nodename[MPI_MAX_PROCESSOR_NAME];
+    cpu_set_t coremask;
+    char clbuf[7 * CPU_SETSIZE];
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);    
+    MPI_Get_processor_name(nodename, &namelen);
+
+    sched_getaffinity(0, sizeof(coremask), &coremask);
+    cpuset_to_cstr(&coremask, clbuf);
+
+    printf("Program <%s>: rank %d on core %s of node <%s>\n", prog, rank, clbuf, nodename);
+}
+
+/* Borrowed from util-linux-2.13-pre7/schedutils/taskset.c */
+void cpuset_to_cstr(cpu_set_t *mask, char *str)
+{
+    char *ptr = str;
+    int i, j, entry_made = 0;
+
+    for (i = 0; i < CPU_SETSIZE; i++)
+      {
+        if (CPU_ISSET(i, mask))
+          {
+            int run = 0;
+            entry_made = 1;
+
+            for (j = i + 1; j < CPU_SETSIZE; j++)
+              {
+                if (CPU_ISSET(j, mask)) run++;
+                else break;
+              }
+
+            if (!run)
+              {
+                sprintf(ptr, "%d,", i);
+              }
+            else if (run == 1)
+              {
+                sprintf(ptr, "%d,%d,", i, i + 1);
+                i++;
+              }
+            else
+              {
+                sprintf(ptr, "%d-%d,", i, i + run);
+                i += run;
+              }
+            while (*ptr != 0) ptr++;
+          }
+      }
+
+    ptr -= entry_made;
+    *ptr = 0;
 }
